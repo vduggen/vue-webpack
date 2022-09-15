5f1fcd8fdeb09d2fc7c578b927b33cb6fc36b514
Vitor Duggen <zduggem@gmail.com>
2022-09-15 17:13:27 -0300

    Criado pasta src/assets e os arquivos App.vue e main.js

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="wrapper">
+      <img src="./assets/logo.png" alt="Logo webpack e vue">
+
+      <h1>
+         Hello world from app <strong>Vue</strong> by <strong>Webpack</strong>
+      </h1>
+   </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.wrapper {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+
+   width: 100%;
+}
+
+.wrapper h1 {
+   font-family: sans-serif;
+}
+
+.wrapper h1 strong:first-child {
+   color: #2fb982;
+}
+
+.wrapper h1 strong:last-child {
+   color: #1c78c0;
+}
+</style>
